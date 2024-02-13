bd8fe92d21269bbb0b74908a8256c0444432351a
sofolitltd <sofolitltd@gmail.com>
2024-02-13 19:57:31 +0600

    init 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import {Poppins } from 'next/font/google'
 
 import './globals.css'
-import Navbar from '@/components/NavBar'
+import Navbar from '@/components/nav-bar'
 import Footer from '@/components/Footer'
 
 
